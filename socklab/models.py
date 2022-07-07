247d0d03093fae913f36933f8905eaaeb5268061
jac-lene <jaclenesini@gmail.com>
2022-07-07 17:14:17 -0400

    model

--- socklab/models.py
+++ socklab/models.py
@@ -1,3 +1,20 @@
+from statistics import mode
+from xmlrpc.client import boolean
 from django.db import models
 
 # Create your models here.
+
+class BasicSock(models.Model):
+    name = models.CharField(max_length=100, default='My Sock Design')
+    CC1 = models.CharField(max_length=100, default = 'white')
+    CC2 = models.CharField(max_length=100, default = 'yellow')
+    CC3 = models.CharField(max_length=100, default = 'red')
+    ankle_height = models.CharField(max_length=100, default = 'crew')
+    foot_length = models.CharField(max_length=100, default = '9.5')
+    foot_stripe = models.BooleanField
+    in_progress = models.BooleanField
+    completed = models.BooleanField
+    user_photo = models.TextField()
+
+    def __str__(self):
+        return self.name
