8910d1d5efb99e9129d5da24a8d09848ab6ed700
jac-lene <jaclenesini@gmail.com>
2022-07-10 20:10:50 -0400

    model updated

--- socklab/models.py
+++ socklab/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class BasicSock(models.Model):
     name = models.CharField(max_length=100, default='My Sock Design')
-    CC1 = models.CharField(max_length=100, default = 'white')
-    CC2 = models.CharField(max_length=100, default = 'yellow', blank=True)
-    CC3 = models.CharField(max_length=100, default = 'red', blank=True)
+    toeColor = models.CharField(max_length=100, default = 'white')
+    ankleColor = models.CharField(max_length=100, default = 'white', blank=True)
+    heelColor = models.CharField(max_length=100, default = 'white', blank=True)
+    footColor = models.CharField(max_length=100, default = 'white', blank=True)
+    ribColor = models.CharField(max_length=100, default = 'white', blank=True)
     ankle_height = models.CharField(max_length=100, default = 'crew')
     foot_length = models.CharField(max_length=100, default = '9.5')
     foot_stripe = models.BooleanField(default=False)
