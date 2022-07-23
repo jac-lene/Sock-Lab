3e6f57ba9ecc28fbb6c162e9e4ae58516f02236e
jac-lene <jaclenesini@gmail.com>
2022-07-22 23:05:54 -0400

    heroku

--- socklab_django/settings.py
+++ socklab_django/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
+    # Use Django'sstandard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
