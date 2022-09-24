b152e1641cb5b9366981cdbe18989f25b44b5338
jac-lene <jaclenesini@gmail.com>
2022-09-24 16:36:57 -0400

    path change

--- socklab/urls.py
+++ socklab/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('socks/', views.SockList.as_view(), name='sock_list'),
+    path('/', views.SockList.as_view(), name='sock_list'),
     path('socks/<int:pk>', views.SockDetail.as_view(), name='sock_detail'),
     path('stash/', views.StashList.as_view(), name='stash_list'),
     path('stash/<int:pk>', views.StashDetail.as_view(), name='stash_detail'),
