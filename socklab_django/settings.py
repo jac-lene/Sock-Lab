556153b46b40091680f2e0ffec9d5c10bea36a5d
jac-lene <jaclenesini@gmail.com>
2022-09-24 16:28:10 -0400

    lalal

--- socklab_django/settings.py
+++ socklab_django/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
-
+import dj_database_url
 
 
 # Builds paths inside the project like this: BASE_DIR / 'subdir'.
